498948c7d3ae0a356bf58c8d8e296d1059767cc3
Reign <reignzj.westry@gmail.com>
2019-06-07 19:53:16 -0700

    git update

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 
 ## Description
     <p>A PHP payment page that accepts credit cards using the stripe API and also stores the data in a MySQL database using PDO. Created tables and models for customers and transactions to store them in the database and retrieve them within the app.</p>
-    
+
+### Project Link
+    https://reignreapsow.github.io/PHP-MYSQL-STRIPE-API-payment-app/    
---
+++ paypage/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Pay Page</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+<div class="container"></div>
+<!--    STRIPE FORM-->
+    <form action="./charge.php" method="post" id="payment-form">
+        <div class="form-row">
+            <label for="card-element">
+            Credit or debit card
+            </label>
+            <div id="card-element form-control">
+            <!-- A Stripe Element will be inserted here. -->
+            </div>
+
+            <!-- Used to display form errors. -->
+            <div id="card-errors" role="alert"></div>
+        </div>
+
+        <button>Submit Payment</button>
+    </form>
+<!--END STRIPE FORM-->
+    <script src="https://js.stripe.com/v3/"></script>
+    <script src="js/charge.js"></script>
+</body>
+</html>
